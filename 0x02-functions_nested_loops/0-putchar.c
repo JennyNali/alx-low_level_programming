372e3ec2bc7acc5f2169fd3ab2353818332a8a52
JennyNali <88007845+JennyNali@users.noreply.github.com>
2022-03-16 09:08:00 +0100

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - Entry point
  *
- * putchar - to print "holberton"
+ * putchar - to print "main"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char holberton[9] = "Holberton";
+char main[4] = "main";
 int i;
-for (i = 0; i < 9; i++)
+for (i = 0; i < 4; i++)
 {
-_putchar(holberton[i]);
+_putchar(main[i]);
 }
 _putchar('\n');
 return (0);
