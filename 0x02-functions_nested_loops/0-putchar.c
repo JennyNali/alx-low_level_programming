79a826726af3fd9d0c5c79897db166bcea6f387e
JennyNali <88007845+JennyNali@users.noreply.github.com>
2022-03-16 09:39:43 +0100

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include "main.h"
+#include "stdio.h"
 
 /**
- * main - Entry point
- *
- * putchar - to print "main"
+ * main - prints _putchar followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char main[4] = "main";
-int i;
-for (i = 0; i < 4; i++)
-{
-_putchar(main[i]);
-}
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
