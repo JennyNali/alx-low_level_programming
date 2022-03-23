05f3c9716ee601b183de9fb98cc2b57418e73474
JennyNali <88007845+JennyNali@users.noreply.github.com>
2022-03-23 15:07:06 +0100

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *leet - Encodes a string into 1337
+ * @str: Pointer to string
+ * Return: Pointer to str
+*/
+char *leet(char *str)
+{
+	char alphas1[6] = "aeotl";
+	char alphas2[6] = "AEOTL";
+	char alphas3[6] = "43071";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == alphas1[j] || str[i] == alphas2[j])
+			{
+				str[i] = alphas3[j];
+			}
+		}
+	}
+	return (str);
+}
