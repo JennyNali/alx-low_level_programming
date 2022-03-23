6c7c2b6beb0b39289ee58c84f493cd1e14c66cce
JennyNali <88007845+JennyNali@users.noreply.github.com>
2022-03-23 15:03:21 +0100

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcmp - Compares two strings
+ * @s1: Pointer to first string
+ * @s2: Pointer to second string
+ * Return: Return -15 if s1 < s2, 15 if s1 > s2, else 0
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; ; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	if (s1[i] == '\0')
+		return (0);
+	}
+}
