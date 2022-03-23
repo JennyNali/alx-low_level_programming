c2f4a14b2cb811945f12be20eb6c096b0981cd86
JennyNali <88007845+JennyNali@users.noreply.github.com>
2022-03-23 15:17:56 +0100

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer >= 0 || <= 0
+ * @n: Integer to print
+ * Return: void
+*/
+void print_number(int n)
+{
+	unsigned int uNum = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		uNum = -uNum;
+	}
+	if ((uNum / 10) > 0)
+		print_number(uNum / 10);
+	_putchar((uNum % 10) + '0');
+}
