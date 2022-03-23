577ba0c21b1449bb19d39e8128351b752720ceb9
JennyNali <88007845+JennyNali@users.noreply.github.com>
2022-03-23 15:04:14 +0100

    rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array - Compares two strings
+ * @a: Pointer to array of integers
+ * @n: Number of elements of a
+ * Return: void
+*/
+void  reverse_array(int *a, int n)
+{
+	int i, *startPt, *endPoint, temp;
+
+	startPt = a;
+	endPoint = a + (n - 1);
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = *startPt;
+		*startPt = *endPoint;
+		*endPoint = temp;
+		startPt++;
+		endPoint--;
+	}
+}
