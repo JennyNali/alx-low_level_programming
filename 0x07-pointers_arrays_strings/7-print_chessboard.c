2176350456c81b05d99e92a5dc18954f7499eadb
JennyNali <88007845+JennyNali@users.noreply.github.com>
2022-03-29 09:27:21 +0100

    update again

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,18 @@
 #include "main.h"
-
 /**
  * print_chessboard - prints the chessboard
  * @a: two dimension array to print
+ * Return: Void
  */
+
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for (j = 0; j < 8; j++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
+		for (i = 0; i < 8; i++)
+			_putchar(a[j][i]);
 		_putchar('\n');
 	}
 }
